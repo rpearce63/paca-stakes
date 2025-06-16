8e88b2a75341fbc0cb527db73b853bdd9f8ed98e
Rick Pearce <rpearce63@mac.com>
2025-06-16 15:54:37 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import StakeViewer from "./StakeViewer";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <StakeViewer />
+    </div>
+  );
+}
+
+export default App;
