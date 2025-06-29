d345ebc282d0fccbd5aeff7a8a5ef9b350fe287e
Rick Pearce <rpearce63@yahoo.com>
2025-06-29 11:12:06 -0500

    Move redirect to component

--- src/components/StakeViewer.jsx
+++ src/components/StakeViewer.jsx
@@ -411,6 +411,13 @@ export default function StakeViewer() {
   return (
     <div className="w-full max-w-5xl mx-auto p-2 sm:p-4 md:p-6 bg-white dark:bg-gray-800 shadow rounded">
       <div className="flex justify-between items-center mb-6">
+        <h3>
+          App domain has changed. Page will redirect to{" "}
+          <a href="https://paca-mw-dashboard.netlify.app">
+            https://paca-mw-dashboard.netlify.app
+          </a>{" "}
+          in 5 seconds
+        </h3>
         <h1 className="text-3xl font-bold text-center">
           Stake Viewer ({NETWORKS[network].name})
         </h1>
